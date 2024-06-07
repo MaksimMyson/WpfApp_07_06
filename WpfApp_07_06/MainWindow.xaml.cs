51f9e032458b728e020f965256c99b3d4b7f7b02
Mayson Kotlyar Maksim <maksimmyson33@gmail.com>
2024-06-07 19:58:01 +0300

    was done 4-5

--- WpfApp_07_06/MainWindow.xaml.cs
+++ WpfApp_07_06/MainWindow.xaml.cs
@@ -11,14 +11,40 @@ using System.Windows.Shapes;
 
 namespace WpfApp_07_06
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            int start = Convert.ToInt32(StartTextBox.Text);
+            int end = Convert.ToInt32(EndTextBox.Text);
+            int threadCount = Convert.ToInt32(ThreadCountTextBox.Text);
+
+            // Створення та запуск потоків
+            for (int i = 0; i < threadCount; i++)
+            {
+                Thread thread = new Thread(() => CountNumbers(start, end));
+                thread.Start();
+            }
+        }
+
+        private void CountNumbers(int start, int end)
+        {
+            // Виведення чисел у вказаному діапазоні
+            for (int i = start; i <= end; i++)
+            {
+                // Виконуємо відображення у UI потоці через Dispatcher
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    ResultTextBox.AppendText(i.ToString() + Environment.NewLine);
+                });
+
+                Thread.Sleep(200); // Затримка для відображення чисел з приривками
+            }
         }
     }
 }
